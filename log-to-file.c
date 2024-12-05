f85f9e310a5c3584d6a176ed1fd8d9437bf2133d
abdomody35 <abdomody35@gmail.com>
2024-12-05 23:13:08 +0200

    program that logs by printing to a file instead of stdin

---
+++ log-to-file.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fd == -1)
+    {
+        perror("open failed");
+        return 1;
+    }
+
+    dup2(fd, 1);
+    dup2(fd, 2);
+
+    printf("Creating log file...\n\n");
+
+    printf("Log file created successfully!\n\n");
+
+    printf("Logging data to file...\n\n");
+
+    printf("Data has been logged to file successfully!\n\n");
+
+    printf("Program terminating...\n");
+
+    close(fd);
+
+    return 0;
+}
